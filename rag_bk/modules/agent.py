1486f031ddde2abc6c786efba74f47e8e26c7507
woowaang <sns_sns123@naver.com>
2025-03-22 23:06:43 +0900

    0322 change

--- rag_bk/modules/agent.py
+++ rag_bk/modules/agent.py
@@ -4,7 +4,7 @@ from langgraph.prebuilt import create_react_agent
 import streamlit as st
 
 
-def create_agent_executor(model_name="gpt-4o", tools=[]):
+def create_agent_executor(model_name="gpt-4o-mini", tools=[]):
     # 메모리 설정
     memory = MemorySaver()
 
@@ -13,34 +13,8 @@ def create_agent_executor(model_name="gpt-4o", tools=[]):
 
     # 시스템 프롬프트 설정
     system_prompt = (
-        """### 💬 중요한 지침: 반드시 이 말투를 유지하세요!
-1. 친근하고 따뜻한 어조로 답변하세요.
-2. 전문성을 유지하면서도 쉬운 언어를 사용하세요. 
-   - (예: "LDL 콜레스테롤 수치가 높다는 것은 쉽게 말해 '나쁜 콜레스테롤'이 많다는 뜻이에요.")
-3. 공감하는 문장을 반드시 포함하세요.
-   - (예: "그런 증상이 나타나면 정말 걱정이 크시겠어요.")
-4. 차분하고 부드러운 어조를 유지하세요.
-   - (예: "이런 증상이 있다면 전문가 상담을 받아보시는 것을 추천드립니다.")
-5. 지나치게 명령형을 사용하지 마세요.  
-   - ❌ "당장 병원에 가세요."
-   - ✅ "이런 증상이 있다면 가능한 한 빨리 전문가 상담을 받아보시는 것이 좋습니다."
-6. 전문적이거나 어려운 용어를 사용할 때는 **무조건** 쉽게 풀어서 설명하는 것을 덧붙이세요.
-   - (예:  “LDL 콜레스테롤 수치가 높다는 것은, 쉽게 말해 '나쁜 콜레스테롤'이 많다는 뜻이에요.”)
-7. 직접 도와주겠다는 문장을 많이 사용하세요.
-   - (예: "산책이 건강에 도움이 될 수 있습니다. 꾸준히 관리하실 수 있도록 제가 도와드릴게요.")
-   
-### 🚫 도구 사용 제한 규칙:
-1. 감정적인 질문이나 위로가 필요한 경우에는 도구를 사용하지 마세요.  
-   - (예: "나 폐암 선고받아서 무서워." → 공감과 위로 제공, 도구 사용 X)
-2. 사용자가 특정 문서를 요청하거나 최신 정보를 요구하는 경우에는는 `pdf_search` 또는 `web_search`를 사용하세요.  
-   - (예: "최근 폐암 치료 연구 결과 알려줘" → `web_search` 사용)
-3. 사용자가 폐암 또는 유방암에 대해 물어보면 `pdf_search`를 우선 사용하세요.
-   
----
-"""
-
         # 생성된 페르소나 프롬프트 입력
-        +f'{st.session_state["new_prompt"]}'
+        f'{st.session_state["new_prompt"]}'
         + """
 
 Here are the tools you can use:
@@ -78,7 +52,7 @@ When you use "search" tool, Please follow these instructions:
 
 For sources:
 - Include all sources used in your report
-- Provide full links to relevant websites
+- Provide full links to relevant websites or specific document paths
 - Separate each source by a newline. Use two spaces at the end of each line to create a newline in Markdown.
 - It will look like:
 
@@ -106,6 +80,5 @@ There should be no redundant sources. It should simply be:
     )
 
     return agent_executor
-
 
 
